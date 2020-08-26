d8eaf4c055f27c244a68b41c7ac8c8d61900d1af
wangao <1366463855@qq.com>
2020-08-26 14:10:27 +0800

    feat: support markdown math render

--- fixtures/mdrender/index.js
+++ fixtures/mdrender/index.js
@@ -3,14 +3,18 @@ const highlight = require("rehype-highlight");
 const html = require("rehype-stringify");
 const remark2rehype = require("remark-rehype");
 const emoji = require("remark-emoji");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 // const sanitize = require("rehype-sanitize");
 
 const mdRender = (content) => {
   return (
     remark()
       .use(emoji)
+      .use(math)
       .use(remark2rehype, { allowDangerousHtml: false })
       // .use(sanitize)
+      .use(katex)
       .use(highlight, { ignoreMissing: true })
       .use(html, { allowDangerousHtml: false })
       .processSync(content).contents
--- src/utils/mdrender.js
+++ src/utils/mdrender.js
@@ -4,15 +4,19 @@ import highlight from "rehype-highlight";
 import html from "rehype-stringify";
 import remark2rehype from "remark-rehype";
 import emoji from "remark-emoji";
+import math from "remark-math";
+import katex from "rehype-katex";
 // import sanitize from "rehype-sanitize";
 
 const mdRender = (content) => {
   return (
     remark()
       .use(emoji)
+      .use(math)
       // .use(emojiA11y)
       .use(remark2rehype, { allowDangerousHtml: false })
       // .use(sanitize)
+      .use(katex)
       .use(highlight, { ignoreMissing: true })
       .use(html, { allowDangerousHtml: false })
       .processSync(content).contents
